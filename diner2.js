ae5bd6f798ce668ad871f8bb0378c499d20d80f0
Maxim Romanov <PoMaHoB.MaKCuM@gmail.com>
2024-05-28 09:46:28 +0200

    improve

--- diner2.js
+++ diner2.js
@@ -4,6 +4,8 @@ const nowork = "nowork";
 const intVal = ["breakfast", "lunch", "diner"];
 const foodType = ["first", "second", "dessert"];
 const error = -1;
+const add = " ";
+const addNum = 1;
 
 let timeInterval = {
   "06:59": nowork,
@@ -15,58 +17,58 @@ let timeInterval = {
 let menu = {
   [intVal[0]]: {
     [foodType[0]]: [
-      { name: "bf01", price: 101 },
-      { name: "bf02", price: 102 },
-      { name: "bf03", price: 103 },
+      { name: "Sandwich", price: 11 },
+      { name: "Tortilla", price: 12 },
+      { name: "Omelette", price: 13 },
     ],
     [foodType[1]]: [
-      { name: "bf11", price: 111 },
-      { name: "bf12", price: 112 },
-      { name: "bf13", price: 113 },
+      { name: "Roll", price: 5 },
+      { name: "Pie", price: 6 },
+      { name: "Croissant", price: 7 },
     ],
     [foodType[2]]: [
-      { name: "bf31", price: 121 },
-      { name: "bf32", price: 122 },
-      { name: "bf33", price: 123 },
+      { name: "Coffee", price: 2 },
+      { name: "Juice", price: 3 },
+      { name: "Tea", price: 1 },
     ],
   },
   [intVal[1]]: {
     [foodType[0]]: [
-      { name: "lc01", price: 201 },
-      { name: "lc02", price: 202 },
-      { name: "lc03", price: 203 },
+      { name: "Soup", price: 14 },
+      { name: "Borsch", price: 15 },
+      { name: "Okroshka", price: 16 },
     ],
     [foodType[1]]: [
-      { name: "lc11", price: 211 },
-      { name: "lc12", price: 212 },
-      { name: "lc13", price: 213 },
+      { name: "Puree", price: 3 },
+      { name: "Cutlet", price: 4 },
+      { name: "Sausage", price: 8 },
     ],
     [foodType[2]]: [
-      { name: "lc31", price: 211 },
-      { name: "lc32", price: 212 },
-      { name: "lc33", price: 213 },
+      { name: "Late", price: 1 },
+      { name: "Coca", price: 2 },
+      { name: "Tea", price: 3 },
     ],
   },
   [intVal[2]]: {
     [foodType[0]]: [
-      { name: "dn01", price: 301 },
-      { name: "dn02", price: 302 },
-      { name: "dn03", price: 303 },
+      { name: "Fish", price: 17 },
+      { name: "Meat", price: 18 },
+      { name: "Seafood", price: 19 },
     ],
     [foodType[1]]: [
-      { name: "dn11", price: 311 },
-      { name: "dn12", price: 312 },
-      { name: "dn13", price: 313 },
+      { name: "Bread", price: 11 },
+      { name: "Apple", price: 10 },
+      { name: "Banana", price: 9 },
     ],
     [foodType[2]]: [
-      { name: "dn31", price: 321 },
-      { name: "dn32", price: 322 },
-      { name: "dn33", price: 323 },
+      { name: "Wine", price: 321 },
+      { name: "Vodka", price: 322 },
+      { name: "Beer", price: 323 },
     ],
   },
 };
 
-const comments = ["random comment 1", "random comment 2", "random comment 3"];
+const comments = ["Very good!", "Keep it going!!", "Give it some more!!!"];
 
 if (toConsole == true) {
   var readlineSync = require("readline-sync");
@@ -146,27 +148,45 @@ function detectInterval(time, intDict) {
   return nowork;
 }
 
+function maxDictMenu(dict) {
+  let size = 0;
+  for (const [key, value] of Object.entries(dict)) {
+    value.forEach(checkValue);
+  }
+
+  function checkValue(value) {
+    if (size < value["name"].length) {
+      size = value["name"].length;
+    }
+  }
+  return size;
+}
+
 function makeMenu(intNow, menuDict) {
   let text = `It is ${intNow} time.\n`;
 
+  let size = maxDictMenu(menuDict) + addNum;
   for (const [key, value] of Object.entries(menuDict)) {
-    text += `${key} `;
+    text += `${key.toUpperCase()}:\n`;
     value.forEach(concatMenu);
     text += "\n";
   }
 
   function concatMenu(food) {
-    text += `${food["name"]} - \$(${food["price"]}) `;
+    text += `${food["name"].toUpperCase()}${add.repeat(size - food["name"].length)} --> ${food["price"]}\$\n`;
   }
 
   return text;
 }
 
 function makeFood(intNow, foodType, menu) {
-  let text = `${intNow} ${foodType}\n`;
+  let text = `Choose your ${foodType} for ${intNow}:\n`;
 
   for (let i = 0; i < menu.length; i++) {
-    text += `${menu[i].name} `;
+    text += `${menu[i].name.toUpperCase()}`;
+    if (i < menu.length - 1) {
+      text += " --OR-- ";
+    }
   }
   text += "\n";
 
@@ -175,29 +195,54 @@ function makeFood(intNow, foodType, menu) {
 
 function checkFood(menu, food) {
   for (let i = 0; i < menu.length; i++) {
-    if (food === menu[i].name) {
+    if (food.toUpperCase() === menu[i].name.toUpperCase()) {
       return i;
     }
   }
   return error;
 }
 
+function maxDictTotal(dict) {
+  let size = 0;
+
+  for (const [key, value] of Object.entries(dict)) {
+    if (size < key.length) {
+      size = key.length;
+    }
+  }
+
+  return size;
+}
+
 function showTotal(choise) {
-  let text = "Your choice\n";
+  let text = "Your choice:\n";
   let total = 0;
 
+  let size = maxDictTotal(choise) + addNum;
   for (const [key, value] of Object.entries(choise)) {
-    text += `${key} - \$${value}\n`;
+    text += `${key}${add.repeat(size - key.length)} --> ${value}\$\n`;
     total += value;
   }
-  text += `Total: ${total}`;
+  text += `Total:${add.repeat(size - 1)}${total}\$`;
 
   return text;
 }
 
+function maxArr(arr) {
+  let size = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (size < arr[i].length) {
+      size = arr[i].length;
+    }
+  }
+
+  return size;
+}
+
 let intNow = nowork;
 do {
-  intNow = detectInterval(input("What time is it now? "), timeInterval);
+  intNow = detectInterval(input("What time is it now? HH:MM "), timeInterval);
   if (intNow != error) {
     if (intNow == nowork) {
       output("Now is not the time to eat.");
@@ -210,6 +255,7 @@ do {
 output(makeMenu(intNow, menu[intNow]));
 
 let choise = {};
+let size = maxArr(foodType);
 for (const [key, value] of Object.entries(menu[intNow])) {
   let tempBool = false;
 
@@ -218,7 +264,9 @@ for (const [key, value] of Object.entries(menu[intNow])) {
     let index = checkFood(value, food);
 
     if (index != -1) {
-      choise[`${key} ${food}`] = menu[intNow][key][index]["price"];
+      choise[
+        `For the ${key.toUpperCase()}${add.repeat(size - key.length)}: ${food.toUpperCase()}`
+      ] = menu[intNow][key][index]["price"];
       tempBool = true;
       output(comments[getRandomInt(comments.length)]);
     }
